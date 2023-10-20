34b2e068e311b9665cdd9a813f51020dea084e60
Edozie <phynnedo@gmail.com>
2023-10-20 15:36:08 +0100

    fix: added some text to the footer section

--- src/app/home/components/footer-section.tsx
+++ src/app/home/components/footer-section.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Button, Typography } from "@/src/ui";
 import Image from "next/image";
 import bgImage from "../services-bg.png";
+import BrownPlayIcon from "@/public/assets/svgs/brown-play-icon.svg";
 
 const FooterSection = () => {
   return (
@@ -16,7 +18,47 @@ const FooterSection = () => {
         />
       </div>
 
-      <div className="p-12 bg-secondary-main"></div>
+      <div className="p-12 bg-secondary-main rounded-[3.13rem]">
+        <div></div>
+        <div>
+          <div>
+            <Button
+              variant="primary"
+              color="primary"
+              label="Join Us Live"
+              leftIcon={<BrownPlayIcon />}
+            />
+            <div></div>
+          </div>
+          <div className="border-secondary-25 border-t h-0 w-full my-8"></div>
+          <div className="flex justify-between">
+            <div>
+              <Typography
+                fontWeight="regular"
+                color = "secondary-25"
+                customClassName="!text-[0.75rem]"
+              >© 2023 LightCity Church. All rights reserved.</Typography>
+            </div>
+            <div className="flex gap-4">
+              <Typography
+                color = "secondary-25"
+                fontWeight="regular"
+                customClassName="!text-[0.75rem]"
+              >Terms</Typography>
+              <Typography
+                color = "secondary-25"
+                fontWeight="regular"
+                customClassName="!text-[0.75rem]"
+              >Privacy</Typography>
+              <Typography
+                color = "secondary-25"
+                fontWeight="regular"
+                customClassName="!text-[0.75rem]"
+              >Cookies</Typography>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
