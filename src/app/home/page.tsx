4a716501eae32eb009f8ef04c1c9632266ee2172
tochukwu670 <toochukwuali@gmail.com>
2023-12-01 09:59:23 +0100

    fixes for the meeting page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -12,10 +12,11 @@ const HomePage = () => {
     <section>
       <HeroSection
         title="WELCOME TO LIGHTCITY CHURCH"
-        subtitle="...where we are committed to giving your life an"
+        subtitle="<h1 class='text-[48px] inline lg:text-[60px] xl:text-[84px] leading-[48px] lg:leading-[80px] mt-3 font-bold'>...where we are <br class='hidden md:block'/> committed to giving your <br class='hidden md:block'/> life an </h1>"
         special="eternal relevance!"
         buttonTitle="Listen Online"
         buttonSvg={<BrownPlayIcon />}
+        buttonIconDirection={'left'}
       />
       <DescriptionSection />
       <SeriesSection />
