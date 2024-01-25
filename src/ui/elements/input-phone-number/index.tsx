ea018872e06f0053de1c833be18b83801df78bef
Edozie <phynnedo@gmail.com>
2024-01-25 18:35:17 +0100

    fix: completed UI implememtation of the special meeting registration form

--- src/ui/elements/input-phone-number/index.tsx
+++ src/ui/elements/input-phone-number/index.tsx
@@ -1,23 +1,34 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import PhoneInput from "react-phone-number-input";
+import "react-phone-number-input/style.css";
+import styles from "./index.module.scss";
 
-interface InputPhoneNumberProps{
-    label: string
+interface InputPhoneNumberProps {
+  label: string;
 }
 
-const InputPhoneNumber: React.FC<InputPhoneNumberProps> = ({
-    label
-}) => {
+const InputPhoneNumber: React.FC<InputPhoneNumberProps> = ({ label }) => {
+  const [value, setValue] = useState<any>();
 
-    return(
-        <div className="flex flex-col mb-3">
-            <label className="cursor-pointer w-fit text-body-reg text-[#3C3C3C] uppercase" htmlFor="">{label}</label>
+  return (
+    <div className="flex flex-col mb-3">
+      <label
+        className="cursor-pointer w-fit text-body-reg text-[#3C3C3C] uppercase"
+        htmlFor=""
+      >
+        {label}
+      </label>
+      <PhoneInput
+        className={styles.numberInput}
+        international
+        defaultCountry="NG"
+        value={value}
+        onChange={setValue}
+      />
+    </div>
+  );
+};
 
-        
-        </div>
-    )
-
-}
-
-export {InputPhoneNumber}
+export { InputPhoneNumber };
