6842d013feb5490dd32563d07137a590f2ec7b5f
--local <phynnedo@gmail.com>
2024-03-25 01:33:50 +0100

    fix: added a hover effect for testimonial buttons

--- src/app/components/nextButton.tsx
+++ src/app/components/nextButton.tsx
@@ -3,7 +3,7 @@ import { useSwiper } from "swiper/react";
 
 const SwiperButtonNext = ({ children }: { children: React.ReactNode }) => {
   const swiper = useSwiper();
-  return <button onClick={() => swiper.slideNext()}>{children}</button>;
+  return <button onClick={() => swiper.slideNext()} className="w-auto h-auto rounded-full cursor-pointer bg-transparent transition ease-in-out delay-75 duration-150 hover:bg-secondary-25">{children}</button>;
 };
 
-export default SwiperButtonNext
+export default SwiperButtonNext;
--- src/app/components/previousButton.tsx
+++ src/app/components/previousButton.tsx
@@ -3,7 +3,7 @@ import { useSwiper } from "swiper/react";
 
 const SwiperButtonPrevious = ({ children }: { children: React.ReactNode }) => {
   const swiper = useSwiper();
-  return <button onClick={() => swiper.slidePrev()}>{children}</button>;
+  return <button onClick={() => swiper.slidePrev()} className="w-auto h-auto rounded-full cursor-pointer bg-transparent transition ease-in-out delay-75 duration-150 hover:bg-secondary-25">{children}</button>;
 };
 
-export default SwiperButtonPrevious
+export default SwiperButtonPrevious;
