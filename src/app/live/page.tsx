1b89d771af91ae3500f871efe36288ab314759e7
Edozie Onyeanusi <phynnedo@gmail.com>
2025-05-14 23:14:33 +0100

    fix: working mixlr link

---
+++ src/app/live/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+
+const Live = () => {
+  return (
+    <section className="w-full h-[100vh] bg-slate-500 flex justify-center items-center">
+      
+      <iframe src="" ></iframe>
+    </section>
+  );
+}
+ 
+export default Live;
