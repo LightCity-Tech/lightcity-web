9ea3c229c584e590ddc6b812afab0bbd3fc7cb8f
Tochukwu Ali <tochukwu.ali@minerva.ng>
2025-05-16 14:56:50 +0100

    change bg

--- src/app/our-meetings/registration/[slug]/page.tsx
+++ src/app/our-meetings/registration/[slug]/page.tsx
@@ -27,7 +27,7 @@ const UpcomingMeeting = () => {
     <section>
       <section
         className="w-full h-[100vh] relative overflow-hidden bg-[url('/assets/images/camp-meeting-hero.webp')] bg-cover bg-center lg:bg-left flex items-center justify-center brightness-80"
-        style={{ backgroundImage: "linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('/assets/images/camp-meeting-hero.webp')" }}
+        style={{ backgroundImage: "linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('/assets/images/pp2-1.jpg') bg-contain bg-no-repeat" }}
       >
         <div className="">
           <Typography
