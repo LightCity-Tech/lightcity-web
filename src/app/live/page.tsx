487f88c8d075671f98ee6ceb3ae7dd6b07a0836e
Edozie Onyeanusi <phynnedo@gmail.com>
2025-05-26 22:50:19 +0100

    feat: working on the url for the sermon dynamic page

--- src/app/live/page.tsx
+++ src/app/live/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import BgImage from "@/public/assets/images/slide02.webp";
+
 const Live = () => {
   return (
-    <section className="w-full h-[100vh] bg-[#F7E7CF] flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center w-[inherit] h-auto">
+    <section className="relative w-full h-[100vh] flex justify-center items-center overflow-hidden">
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat filter blur-sm"
+        style={{
+          backgroundImage: `url(${BgImage.src})`,
+        }}
+      ></div>
+      <div className="relative z-10 flex flex-col justify-center items-center w-[inherit] h-auto">
         <iframe
           src="https://pastortochilightcitychurch.mixlr.com/embed"
-          className="w-[60%] h-[36rem] border-0 rounded-[20px] shadow-xl"
+          className="w-[60%] h-[36rem] border-0 rounded-[20px] shadow-xl mobile:w-[75%] small-mobile:!w-[90%]"
         ></iframe>
       </div>
     </section>
